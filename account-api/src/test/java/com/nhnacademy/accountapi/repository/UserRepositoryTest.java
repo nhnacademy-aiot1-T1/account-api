fa727f7714397bc174a94cdc75edf0cb01a9bed9
{qaw302} <{osshun21@gmail.com}>
2024-04-07 01:06:08 +0900

    feat: test code

--- account-api/src/test/java/com/nhnacademy/accountapi/repository/UserRepositoryTest.java
+++ account-api/src/test/java/com/nhnacademy/accountapi/repository/UserRepositoryTest.java
@@ -1,15 +1,5 @@
 package com.nhnacademy.accountapi.repository;
 
-import static com.nhnacademy.accountapi.domain.User.AuthType.DIRECT;
-import static com.nhnacademy.accountapi.domain.User.UserStatus.ACTIVE;
-import static org.assertj.core.api.Assertions.as;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import com.nhnacademy.accountapi.domain.User;
-import com.nhnacademy.accountapi.domain.User.AuthType;
-import com.nhnacademy.accountapi.domain.User.UserRole;
-import com.nhnacademy.accountapi.domain.User.UserStatus;
-import com.nhnacademy.accountapi.domain.UserAuth;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
