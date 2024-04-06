37a98139dfb38b62b88dcbfb02cf5ef6cf51b949
{qaw302} <{osshun21@gmail.com}>
2024-04-07 01:37:07 +0900

    feat: test code

--- account-api/src/test/java/com/nhnacademy/accountapi/repository/UserRepositoryTest.java
+++ account-api/src/test/java/com/nhnacademy/accountapi/repository/UserRepositoryTest.java
@@ -1,5 +1,12 @@
 package com.nhnacademy.accountapi.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.nhnacademy.accountapi.domain.User;
+import com.nhnacademy.accountapi.domain.User.AuthType;
+import com.nhnacademy.accountapi.domain.User.UserRole;
+import com.nhnacademy.accountapi.domain.User.UserStatus;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,18 +24,47 @@ class UserRepositoryTest {
   @Test
   @DisplayName("유저 등록")
   void createUser() {
+    User user = User.builder()
+        .id(1L)
+        .authType(AuthType.DIRECT)
+        .name("userName")
+        .email("user@user")
+        .status(UserStatus.ACTIVE)
+        .role(UserRole.USER)
+        .build();
 
+    User result = userRepository.save(user);
+
+    assertThat(result).isNotNull();
+    assertThat(result.getId()).isNotNull();
+    assertThat(result.getName()).isEqualTo("userName");
+    assertThat(result.getEmail()).isEqualTo("user@user");
+    assertThat(result.getStatus()).isEqualTo(UserStatus.ACTIVE);
+    assertThat(result.getRole()).isEqualTo(UserRole.USER);
   }
 
   @Test
   @DisplayName("유저 조회 - pk")
   void findById() {
+    User user = User.builder()
+        .id(1L)
+        .authType(AuthType.DIRECT)
+        .name("userName")
+        .email("user@user")
+        .status(UserStatus.ACTIVE)
+        .role(UserRole.USER)
+        .build();
 
+    userRepository.save(user);
+
+    User result = userRepository.findById(user.getId()).orElse(null);
+
+    assertThat(result).isNotNull();
+    assertThat(result.getId()).isEqualTo(result.getId());
+    assertThat(result.getName()).isEqualTo(user.getName());
+    assertThat(result.getEmail()).isEqualTo(user.getEmail());
+    assertThat(result.getStatus()).isEqualTo(UserStatus.ACTIVE);
+    assertThat(result.getRole()).isEqualTo(UserRole.USER);
   }
 
-  @Test
-  @DisplayName("유저 삭제")
-  void deleteUser() {
-
-  }
 }
