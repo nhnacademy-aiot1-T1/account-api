2c86cad98ad214eac3e66a8500ae1542af116ed5
{qaw302} <{osshun21@gmail.com}>
2024-04-19 22:08:57 +0900

    fix : test 코드 지움

--- account-api/src/test/java/com/nhnacademy/accountapi/repository/AccountAuthRepositoryTest.java
+++ account-api/src/test/java/com/nhnacademy/accountapi/repository/AccountAuthRepositoryTest.java
@@ -8,11 +8,13 @@ import com.nhnacademy.accountapi.domain.AccountAuth;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 @DataJpaTest
-//@AutoConfigureTestDatabase(replace = Replace.NONE)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 class AccountAuthRepositoryTest {
   @Autowired
   TestEntityManager entityManager;
@@ -23,24 +25,13 @@ class AccountAuthRepositoryTest {
   @Test
   @DisplayName("유저 등록")
   void createUser() {
-    AccountAuth accountAuth = new AccountAuth(1L, "userId", "password");
-    AccountAuth result = accountAuthRepository.save(accountAuth);
 
-    assertThat(result.getId()).isEqualTo(accountAuth.getId());
-    assertThat(result.getLoginId()).isEqualTo(accountAuth.getLoginId());
-    assertThat(result.getPassword()).isEqualTo(accountAuth.getPassword());
 
   }
 
   @Test
   @DisplayName("유저 id로 존재여부 체크")
   void findById() {
-    Account account = Account.builder().authType(AuthType.DIRECT).build();
-    entityManager.persist(account);
 
-    AccountAuth accountAuth = new AccountAuth(1L, "userId", "password");
-    accountAuthRepository.save(accountAuth);
-
-    assertThat(accountAuthRepository.existsByLoginId(accountAuth.getLoginId())).isTrue();
   }
 }
