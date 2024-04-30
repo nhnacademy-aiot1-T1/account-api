71072ed66ea54e9a61e756b2a93720f8c9139b18
{qaw302} <{osshun21@gmail.com}>
2024-04-30 21:13:19 +0900

    feat : AccountOAuth 기능 추가

---
+++ account-api/src/test/java/com/nhnacademy/accountapi/controller/OAuthControllerTest.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.accountapi.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class OAuthControllerTest {
+
+  @Test
+  void getAccountInfo() {
+  }
+
+  @Test
+  void registerAccount() {
+  }
+}
---
+++ account-api/src/test/java/com/nhnacademy/accountapi/repository/AccountOAuthRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.nhnacademy.accountapi.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class AccountOAuthRepositoryTest {
+
+  @Test
+  void findByOauthId() {
+  }
+
+  @Test
+  void existsByOauthId() {
+  }
+}
